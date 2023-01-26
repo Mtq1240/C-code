84bd584ba8a527f10ff5555659dd3e0c7b47b1ba
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-01-26 20:27:19 +0800

    Update 数组定义初始化作业.c

--- 数组定义初始化作业.c
+++ 数组定义初始化作业.c
@@ -8,3 +8,54 @@ int main()
 	//strlen - 函数-求字符串长度,找\0之前出现的字符个数
 	return 0;
 }
+
+
+
+
+//实现函数init()初始化数组为0
+//实现print()打印数组的每个元素
+//实现reverse()函数完成数组元素的逆置
+void init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = 0;
+	}
+}
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz ; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+void reverse(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	print(arr, sz);
+	reverse(arr, sz);
+	print(arr,sz); 
+	init(arr, sz);
+	print(arr, sz);
+	return 0;
+}
