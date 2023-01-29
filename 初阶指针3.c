c5fa1308527f6ad71f2f1635df7c779b377fbcdd
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-01-29 21:06:13 +0800

    数组和指针

--- 初阶指针3.c
+++ 初阶指针3.c
@@ -7,3 +7,25 @@ int main()
 	printf("%p\n", &arr[0]);
 	return 0;
 }
+
+
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+	int* p = arr;
+	printf("%d\n", arr[2]);
+	printf("%d\n", p[2]);
+	
+	//[] 是一个操作符 2和arr是两个操作数
+	//a+b
+	//b+a
+	printf("%d\n", 2[arr]);
+	printf("%d\n", arr[2]);
+
+	//arr[2] -->*(arr+2) -->*(2+arr)
+
+	//arr[2]<==>*(arr+2)<==>*(p+2)<==>*(2+p)<==>*(2+arr)==2[arr]
+	//2[arr]<==>*(2+arr)
+	return 0;
+}
