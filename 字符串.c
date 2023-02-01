a4567905c5db66e43165680af90caee8a97613a1
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-01 21:54:58 +0800

    Update and rename 字符串 to 字符串.c

--- 字符串.c
+++ 字符串.c
@@ -1,4 +1,4 @@
-1.编写一个将一个字符串逆转的程序, 如将a[] = “apple“改为a[] = “elppa“。
+//1.编写一个将一个字符串逆转的程序, 如将a[] = “apple“改为a[] = “elppa“。
 
 #include<stdio.h>
 #include<string.h>
@@ -40,7 +40,41 @@ int main()
 	return 0;
 }
 
-2.(2)折半查找算法
+//2.(2)折半查找算法
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 8;
+
+	
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+
+	while (left<=right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid+1 ;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid -1;
+		}
+		else
+		{
+			printf("找到了,下标是:%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
 
 
 
