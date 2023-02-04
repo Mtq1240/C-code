77915af8a1af91ab7483fd2dc60b346f70091aa5
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-04 17:44:55 +0800

    Update 进阶指针1.c

--- 进阶指针1.c
+++ 进阶指针1.c
@@ -19,3 +19,28 @@ int main()
 		printf("str3 and str4 are not same\n");
 	return 0;
 }
+
+
+//数组指针
+#include<stdio.h>
+int main()
+{
+	int a[5] = { 1,2,3,4,5 };
+	int b[5] = { 2,3,4,5,6 };
+	int c[5] = { 3,4,5,6,7 };
+
+	int* arr[3] = { a,b,c };
+
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			/*printf("%d ", *(arr[i] + j));*/
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
