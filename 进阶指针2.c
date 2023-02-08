ae9187f12f3447849fa959c15b785a7c8c63c830
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-08 22:05:48 +0800

    函数指针

--- 进阶指针2.c
+++ 进阶指针2.c
@@ -59,3 +59,92 @@ int main()
 		//signal是一个函数的声明
 	return 0;
 }
+
+//------------------------------------------------------------------------------------
+//函数指针数组 - 存放函数指针的数组
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int main()
+{
+	int (*p1)(int, int) = Add;
+	int (*p2)(int, int) = Sub;
+	int(*pArr[2])(int, int) = { Add,Sub };//pArr就是函数指针数组
+
+	return 0;
+}
+
+
+
+//------------------------------------------------------------------------------------
+//计算器
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int sub(int x, int y)
+{
+	return x - y;
+}
+
+int mul(int x, int y)
+{
+	return x * y;
+}
+
+int div(int x, int y)
+{
+	return x / y;
+}
+
+void menu()
+{
+	printf("************************\n");
+	printf("**** 1. add  2. sub ****\n");
+	printf("**** 3. mul  4. div ****\n");
+	printf("*******  0. exit *******\n");
+	printf("************************\n");
+}
+
+#include<stdio.h>
+int main()
+{
+	//计算器-计算整型变量的加、减、乘、除
+	int input = 0;
+	do
+	{
+		int(*pfArr[5])(int, int) = { NULL,add,sub,mul,div };
+		menu();
+		int x = 0;
+		int y = 0;
+		printf("请选择:\n");
+		scanf("%d", &input);
+		if (input >= 1 && input <= 4)
+		{
+			printf("请输入两个数:\n");
+			scanf("%d %d", &x, &y);
+			int ret = (pfArr[input])(x, y);
+			printf("%d\n", ret);
+		}
+		else if (input == 0)
+		{
+			printf("退出程序\n");
+			break;
+		}
+		else
+		{
+			printf("重新输入\n");
+		}
+	} while (input);
+	return 0;
+}
+
