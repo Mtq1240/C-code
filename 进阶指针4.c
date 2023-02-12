9fc76fe55f882e994047888f245b7baac45d715f
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-12 19:14:59 +0800

    ,回调函数,模仿qsort实现冒泡排序通用算法

--- 进阶指针4.c
+++ 进阶指针4.c
@@ -7,19 +7,7 @@ void print_arr(int* arr, int sz)
 		printf("%d ", arr[i]);
 	}
 }
-int cmp_int(const void* e1, const void* e2)
-{
-	return*(int*)e1 - *(int*)e2;
-}
-void test3()
-{
-	int arr[] = {1,3,5,7,9,2,4,6,8,0};
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
-	print_arr(arr, sz);
-}
-
-void swap(char*buf1,char*buf2,int width)
+void swap(char* buf1, char* buf2, int width)
 {
 	int i = 0;
 	for (i = 0; i < width; i++)
@@ -33,7 +21,7 @@ void swap(char*buf1,char*buf2,int width)
 }
 void bubble_sort(void* base, int sz, int width, int(*cmp)(const void* e1, const void* e2))
 {
-	int i = 0; 
+	int i = 0;
 	//趟数
 	for (i = 0; i < sz - 1; i++)
 	{
@@ -42,14 +30,26 @@ void bubble_sort(void* base, int sz, int width, int(*cmp)(const void* e1, const 
 		for (j = 0; j < sz - 1; j++)
 		{
 			//两元素比较
-			if (cmp((char*)base+j*width,(char*)base+(j+1)*width) > 0)
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
 			{
 				//交换
-				swap((char*)base + j * width, (char*)base + (j + 1) * width,width);
+				swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
 			}
 		}
 	}
 }
+int cmp_int(const void* e1, const void* e2)
+{
+	return*(int*)e1 - *(int*)e2;
+}
+void test3()
+{
+	int arr[] = {1,3,5,7,9,2,4,6,8,0};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+	print_arr(arr, sz);
+}
+
 int main()
 {
 	test3();
