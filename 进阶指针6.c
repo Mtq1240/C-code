7eacf6c48e5a1dc0aad87e397c9c9e39145d3e27
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-16 23:20:41 +0800

    Update 进阶指针6.c

--- 进阶指针6.c
+++ 进阶指针6.c
@@ -95,3 +95,20 @@ int main()
 	printf("%s\n", *pa);//at
 	return 0;
 }
+
+
+//8.
+#include<stdio.h>
+int main()
+{
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+
+	printf("%s\n", **++cpp);//POINT
+	printf("%s\n", *-- * ++cpp + 3);//
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+
+	return 0;
+}
