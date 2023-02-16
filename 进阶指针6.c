4fc129848cf2b188820cc9fed32c9512945c2ac0
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-16 22:21:36 +0800

    Update 进阶指针6.c

--- 进阶指针6.c
+++ 进阶指针6.c
@@ -70,3 +70,15 @@ int main()
 	
 	return 0;
 }
+
+
+//6.
+#include<stdio.h>
+int main()
+{
+	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* ptr1 = (int*)(&aa + 1);
+	int* ptr2 = (int*)(*(aa + 1));
+	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));//10  5
+	return 0;
+}
