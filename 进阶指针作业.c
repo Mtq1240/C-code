6148980335f4ae2fb33aa22329d7700e817101cc
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-17 20:12:56 +0800

    Update 进阶指针作业.c

--- 进阶指针作业.c
+++ 进阶指针作业.c
@@ -10,3 +10,14 @@ C.    int (*(*p)[10])(int*)
 D.    int((int*)[10])* p
 
 
+//2.
+//定义一个函数指针，指向的函数有两个int形参并且返回一个函数指针，返回的指针指向一个有一个int形参且返回int的函数？下面哪个是正确的？（ A  ）
+A.    int (*(*F)(int, int))(int)
+
+B.    int (*F)(int, int)
+
+C.    int (*(*F)(int, int))
+
+D.    *(*F)(int, int)(int)
+
+//3.
