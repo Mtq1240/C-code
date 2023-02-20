dd3a1d746b24cccd7fb47b48df4f1ca2b2f1ed7d
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-20 23:04:37 +0800

    Update 进阶指针作业1.c

--- 进阶指针作业1.c
+++ 进阶指针作业1.c
@@ -11,3 +11,26 @@ int findRound(const char * src, char * find)
 	strcat(tmp, src); //再连接一遍
 	return strstr(tmp, find) != NULL; //看看找不找得到
 }
+
+
+
+//字符串左旋
+//实现一个函数，可以左旋字符串中的k个字符。
+//例如：ABCD左旋一个字符得到BCDA
+//ABCD左旋两个字符得到CDAB
+
+void leftRound(char * src, int time)
+{
+	int i, j, tmp;
+  int len = strlen(src);
+  time %= len; //长度为5的情况下，旋转6、11、16...次相当于1次，7、12、17...次相当于2次，以此类推。
+	for (i = 0; i < time; i++) //执行k次的单次平移
+	{
+		tmp = src[0];
+		for(j = 0; j < len - 1; j++) //单次平移
+		{
+			src[j] = src[j + 1];
+		}
+		src[j] = tmp;
+	}
+}
