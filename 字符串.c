c1c098064b154740c38ddede90b3d72543c4df2d
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-22 22:56:30 +0800

    Create 字符串.c

--- 字符串.c
+++ 字符串.c
@@ -127,7 +127,37 @@ int main()
 }
 
 
-
+//字符串左旋
+//实现一个函数，可以左旋字符串中的k个字符。
+//例如：ABCD左旋一个字符得到BCDA
+//ABCD左旋一个字符得到BCDA
+//ABCD左旋两个字符得到CDAB
+#include<stdio.h>
+void string_left(char*str,int k)
+{
+	int n = strlen(str);
+	for (int i = 0; i < k; i++)
+	{
+		//1.每次旋转一个字符
+		char tmp = *str;
+		//2.把后面的n-1个字符向前挪动
+		int j = 0;
+		for (j = 0; j < n - 1; j++)
+		{
+			*(str + j) = *(str + j + 1);
+		}
+		//3.tmp放在最后
+		*(str + n - 1) = tmp;
+	}
+}
+int main()
+{
+	char arr[] = "ABCDEF";
+	int k = 2;
+	string_left(arr,k);
+	printf("%s\n", arr);
+	return 0;
+}
 
 
 
