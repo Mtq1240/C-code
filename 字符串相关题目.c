7ea8d9800d0ca4878a606123bd03abd58fe25354
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-02-24 16:17:25 +0800

    Update 字符串相关题目.c

--- 字符串相关题目.c
+++ 字符串相关题目.c
@@ -79,6 +79,8 @@ int main()
 
 
 
+
+
 //倒置字符串
 //i like beijing.
 //beijing.like i
@@ -127,11 +129,16 @@ int main()
 }
 
 
+
+
+
 //字符串左旋
 //实现一个函数，可以左旋字符串中的k个字符。
 //例如：ABCD左旋一个字符得到BCDA
 //ABCD左旋一个字符得到BCDA
 //ABCD左旋两个字符得到CDAB
+
+//法1
 #include<stdio.h>
 void string_left(char*str,int k)
 {
@@ -160,4 +167,38 @@ int main()
 }
 
 
+//法2
+#include<stdio.h>
+#include<assert.h>
+void reverse(char* left, char* right)
+{
+	assert(left);
+	assert(right);
+	while (left < right)
+	{
+		char tmp = 0;
+		tmp = *(left);
+		*(left) = *(right);
+		*(right) = tmp;
+		left++;
+		right--;
+	}
+}
+void string_left(char*str,int k)
+{
+	assert(str);
 
+	//三步翻转法
+	int n = strlen(str);
+	reverse(str, str + k - 1);//AB逆序
+	reverse(str + k, str + n - 1);//CDEF逆序
+	reverse(str, str + n - 1);//整体逆序
+}
+int main()
+{
+	char arr[] = "ABCDEF";
+	int k = 4;
+	string_left(arr, k);
+	printf("%s\n", arr);
+	return 0;
+}
