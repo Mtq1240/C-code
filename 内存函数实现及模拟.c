5fc76b484bf9616156da021b375e96b5ca9f2fc8
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-03-03 18:03:53 +0800

    Create 内存函数实现及模拟.c

---
+++ 内存函数实现及模拟.c
@@ -0,0 +1,24 @@
+//1.
+//memcpy : 内存拷贝(拷贝不重叠的函数)
+//void*memcpy(void*destination,const void*source,size_t num)
+#include<stdio.h>
+#include<assert.h>
+void* my_memcpy(void* dest, void* src, size_t num)
+{
+	void* ret = dest;
+	assert(dest && src);
+	while (num--)
+	{
+		*(char*)dest = *(char*)src;
+		dest = (char*)dest + 1;
+		src = (char*)src + 1;
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr2[10] = { 0 };
+	my_memcpy(arr2, arr1, 20);
+	return 0;
+}
