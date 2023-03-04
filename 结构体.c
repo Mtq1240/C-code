607572594d6daba712b800c52a6617436595ce05
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-03-04 19:27:58 +0800

    Update 结构体.c

--- 结构体.c
+++ 结构体.c
@@ -6,3 +6,26 @@ struct Node
 	int date;
 	struct Node* next; 
 };
+
+
+
+//2.结构体变量的定义和初始化
+struct S
+{
+	char c;
+	int i;
+}s1,s2;
+
+struct B
+{
+	double d;
+	struct S s;
+	char c;
+};
+int main()
+{
+	struct B sb = { 3.14,{'w',100},'q' };
+
+	printf("%lf %c %d %c\n", sb.d, sb.s.c, sb.s.i, sb.c);
+	return 0;
+}
